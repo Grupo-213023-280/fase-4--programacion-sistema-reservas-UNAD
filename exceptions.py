84902625ea617836426b12deed17a167b10a5646
Farid Camilo Buritica Buitrago <farydd283@gmail.com>
2026-05-11 17:15:13 -0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,52 +1,16 @@
-# ============================================================
-# INFORMACION DEL COMPONENTE PRACTICO  Ejercicio 1
-# Fase 4 Componente Práctico-Practicas Simuladas Sistema de Gestion de clienetes, Servicios y Reservas 
-# Curso: Programación - UNAD 213023_280
-# Autor: William Beltrán
-# Claudia Lorena Franco
-# Farid Camilo Buitrago
-# ============================================================
-
-# ============================================
-# NATIONAL OPEN AND DISTANCE UNIVERSITY (UNAD)
-# COURSE: PROGRAMMING
-# ACTIVITY: PHASE 4 - OOP
-# STUDENT: William Beltrán
-# Claudia Lorena Franco
-# Farid Camilo Buitrago
-# ============================================
-
-# Practical Exercise - Simulated Practices: Customer, Service, and Reservation Management System
-# Course: Programming - UNAD 213023_280
-# Author: William Beltrán
-# Claudia Lorena Franco
-# Farid Camilo Buitrago
-# Concepts applied:
-# including custom exceptions, use of try/except, try/except/else, try/except/finally blocks, and exception chaining.
-
-
-import tkinter as tk
-
-# ============================================
-# BASE CLASS (ENCAPSULATION)
-# ============================================
-
 """
-Custom exceptions module
-Defines specific system errors
+Sistema Integral de Gestión de Clientes, Servicios y Reservas - Software FJ
+Curso: Programación 213023
+Universidad Nacional Abierta y Distancia - UNAD
+Archivo: excepciones.py
+Autor: Farid Camilo Buritica Buitrago
+Descripción: Excepciones personalizadas
 """
 
+class SistemaException(Exception):
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
 
-class ClientError(Exception):
-    """Raised when client data is invalid"""
-    pass
-
-
-class ServiceError(Exception):
-    """Raised when a service is invalid"""
-    pass
-
-
-class ReservationError(Exception):
-    """Raised when reservation cannot be processed"""
+class DatosInvalidosError(SistemaException):
     pass
